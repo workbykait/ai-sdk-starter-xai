12c830d0d92a1d0efe51229bbc8773f4a16b9209
workbykait <workbykait@proton.me>
2025-04-18 02:02:55 -0400

    Update providers.ts

--- ai/providers.ts
+++ ai/providers.ts
@@ -2,11 +2,7 @@ import { xai } from "@ai-sdk/xai";
 import { customProvider } from "ai";
 
 const languageModels = {
-  "grok-2-1212": xai("grok-2-1212"),
-  "grok-3": xai("grok-3-latest"),
-  "grok-3-fast": xai("grok-3-fast-latest"),
-  "grok-3-mini": xai("grok-3-mini-latest"),
-  "grok-3-mini-fast": xai("grok-3-mini-fast-latest"),
+  "grok-3": xai("grok-3-latest")
 };
 
 export const model = customProvider({
