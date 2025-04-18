a1d3c5871679f6210f4a9ca17b43d774dcdb2a45
workbykait <workbykait@proton.me>
2025-04-18 13:12:24 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="fixed right-0 left-0 w-full top-0  text-[#303030]">
+    <div className="fixed right-0 left-0 w-full top-0 text-[#303030]">
       <div className="flex justify-between items-center p-4">
         <div className="flex flex-row items-center gap-4">
-          <Link href="/" className="text-2xl hover:text-[#E0E0E0]">
+          <Link href="/chat" className="text-2xl hover:text-[#E0E0E0]">
             💬
           </Link>
           <Link href="/profile" className="text-2xl hover:text-[#E0E0E0]">
