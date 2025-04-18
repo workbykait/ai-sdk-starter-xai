17cf59f37bdc75383f8e349893b623818183a198
workbykait <workbykait@proton.me>
2025-04-18 15:10:48 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { Header } from "@/components/header";
-import { Analytics } from "@vercel/analytics/react"
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
