b747f7c40660911fab5d4fb695db8aff164d564d
workbykait <workbykait@proton.me>
2025-04-18 12:43:51 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-[#303030]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
       >
         <Header />
         <main>{children}</main>
